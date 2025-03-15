c0bf4f7126efacff6af1db0f773709d4b98991df
gilangrizkiputra <gilangrizkiptra@gmail.com>
2025-03-15 13:40:13 +0700

    feat: add theme and image

--- app/src/main/java/com/project/phistingdetection/ui/theme/Color.kt
+++ app/src/main/java/com/project/phistingdetection/ui/theme/Color.kt
@@ -9,3 +9,14 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+
+val blue = Color(0xFF378CE7)
+val turquoiseGreen = Color(0xFF003950)
+val black = Color(0xFF000000)
+val white = Color(0xFFFFFFFF)
+val green = Color(0xFF05FF00)
+val red = Color(0xFFFF1C1C)
+val grey = Color(0xFFF4F2F7)
+val darkGrey = Color(0xFF8A8A8E)
+
--- app/src/main/java/com/project/phistingdetection/ui/theme/Theme.kt
+++ app/src/main/java/com/project/phistingdetection/ui/theme/Theme.kt
@@ -1,3 +1,5 @@
+@file:Suppress("DEPRECATION")
+
 package com.project.phistingdetection.ui.theme
 
 import android.app.Activity
@@ -9,7 +11,11 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
@@ -48,6 +54,18 @@ fun PhistingDetectionTheme(
 
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
+    }
+
+    val view = LocalView.current
+    if (!view.isInEditMode){
+        SideEffect {
+            val window = (view.context as Activity).window
+            window.statusBarColor = blue.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars
+
+            window.navigationBarColor = blue.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightNavigationBars = true
+        }
     }
 
     MaterialTheme(
